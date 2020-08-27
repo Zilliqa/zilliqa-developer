bd772db1eeff0edcba2b8ccfbf199630e121655c
joel-zilliqa <joel@zilliqa.com>
2020-08-27 16:33:08 +0800

    Added output redirection to console.

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 echo $MODE
 if [ "$MODE" == "load" ]; then
     echo "loading persistence."
-    isolatedServer -l -t 5000 >> /zilliqa/logs/isolated-server.logs
+    isolatedServer -l -t 5000 &>> /zilliqa/logs/isolated-server.logs
 else
     echo "loading from file."
-    isolatedServer -t 5000 -f boot.json >> /zilliqa/logs/isolated-server.logs
+    isolatedServer -t 5000 -f boot.json &>> /zilliqa/logs/isolated-server.logs
 fi
