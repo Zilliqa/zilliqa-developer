17ed10da7ba6bbb4cff96dbfa1077093e72d8e11
joel-zilliqa <joel@zilliqa.com>
2020-09-15 19:14:26 +0800

    remove custom ecr login script.

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -14,7 +14,8 @@ regionID=us-west-2
 application=zilliqa-isolated-server
 registryURL=${accountID}.dkr.ecr.${regionID}.amazonaws.com/$application
 
-eval "$(aws ecr get-login --no-include-email --region $regionID --registry-ids $zilliqa_ecr_id)"
+# Uncomment below if we're pulling from zilliqa ecr
+#eval "$(aws ecr get-login --no-include-email --region $regionID --registry-ids $zilliqa_ecr_id)"
 
 docker build -t "$registryURL:latest" -t "$registryURL:$commit" .
 eval "$(aws ecr get-login --no-include-email --region $regionID)"
