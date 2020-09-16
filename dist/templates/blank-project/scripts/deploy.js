2c808823bdaa000aed86adbbaf28e6d17eaad29c
Ovidiu Miclea <ovidiualx@gmail.com>
2020-09-16 15:46:24 +0300

    add blank-project

--- dist/templates/blank-project/scripts/deploy.js
+++ dist/templates/blank-project/scripts/deploy.js
@@ -1,21 +1,18 @@
-/** Internal stuff scilla-boilerplate needs to deploy */
 require('dotenv').config()
 var fs = require('fs');
 var path = require('path');
-const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8'));
 
 const NETWORK_URL = process.env.NETWORK_URL;
 const ACCOUNT_PRIVATEKEY = process.env.ACCOUNT_PRIVATEKEY;
 
 const ZilTest = require('zilliqa-testing-library').ZilTest;
-/** / Internal stuff scilla-boilerplate needs to deploy */
 
 async function deploy() {
     const ZT = new ZilTest(NETWORK_URL);
 
     await ZT.importAccounts([ACCOUNT_PRIVATEKEY]);
 
-    const contractCode = fs.readFileSync(path.join(__dirname, '../' + packageJson.scillaFile), 'utf8');
+    const contractCode = fs.readFileSync(path.join(__dirname, '../<%= projectName %>'), 'utf8');
 
     const init = JSON.parse(fs.readFileSync(path.join(__dirname, '../init.json'), 'utf8'));
 
