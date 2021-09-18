f8332a298c5e5aafcf4050eb8494c5685aaa689f
joel-zilliqa <joel@zilliqa.com>
2021-09-18 10:51:33 +0000

    Updated Isolated server for v8.1.0 release

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@
 echo $MODE
 if [ "$MODE" == "load" ]; then
     echo "loading persistence."
-    isolatedServer -l -t 5000 | tee /zilliqa/logs/isolated-server.log
+    isolatedServer -l -t 5000 -u "$UUID" | tee /zilliqa/logs/isolated-server.log
 else
     echo "loading from file."
-    isolatedServer -t 5000 -f boot.json | tee /zilliqa/logs/isolated-server.log
+    echo "UUID is: $UUID"
+    isolatedServer -t 5000 -f boot.json -u "$UUID" | tee /zilliqa/logs/isolated-server.log
 fi
