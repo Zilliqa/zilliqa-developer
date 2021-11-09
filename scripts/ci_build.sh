3fd1f077efb832b3d6d673d2b36092d65f0a719e
Joel Lim <56012934+joel-zilliqa@users.noreply.github.com>
2021-11-09 16:50:21 +0800

    Update ci_build.sh

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -11,7 +11,7 @@ commit=$(git rev-parse --short=7 $TRAVIS_COMMIT)
 accountID=$(aws sts get-caller-identity --output text --query 'Account')
 regionID=us-west-2
 
-application=zilliqa-stats
+application=zilliqa-stats-api
 registryURL=${accountID}.dkr.ecr.${regionID}.amazonaws.com/$application
 
 docker build -t "zilliqa/$application:latest" -t "zilliqa/$application:$commit" -t "$registryURL:latest" -t "$registryURL:$commit" .
