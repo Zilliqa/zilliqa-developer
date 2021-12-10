538b518df356da2b1daaedf28bc8ae6704406528
joel-zilliqa <joel@zilliqa.com>
2021-12-10 08:26:48 +0000

    Added manual mode to isolated server launch.

--- run.sh
+++ run.sh
@@ -11,7 +11,9 @@ echo $VALIDATOR_MODE
 echo $T
 echo $UUID
 
-if [ "$VALIDATOR_MODE" == "true" ]; then
+if [ "$MANUAL_MODE" == "true" ]; then
+    isolatedServer -f boot.json -u "$UUID" | tee /zilliqa/logs/isolated-server.log
+elif [ "$VALIDATOR_MODE" == "true" ]; then
     echo "Running in validator mode."
     ./validate.sh
 else
