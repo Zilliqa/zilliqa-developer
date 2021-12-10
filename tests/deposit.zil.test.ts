f80d196c6c53cfc19fdc2ecbf618994b1e5fedde
xiaohuo <xiaohuo200@gmail.com>
2021-12-10 13:45:58 +0800

    refactor: deposit testing

--- tests/deposit.zil.test.ts
+++ tests/deposit.zil.test.ts
@@ -3,13 +3,12 @@ import fs from "fs";
 import { getAddressFromPrivateKey, schnorr } from "@zilliqa-js/crypto";
 
 import {
-  useContractInfo,
   increaseBNum,
-  getContractInfo
+  getJSONParams
 } from "./testutil";
 
 import {
-  CONTAINER,
+  //CONTAINER,
   API,
   TX_PARAMS,
   CONTRACTS,
@@ -22,11 +21,6 @@ const GENESIS_PRIVATE_KEY = global.GENESIS_PRIVATE_KEYS[JEST_WORKER_ID - 1];
 
 const zilliqa = new Zilliqa(API);
 zilliqa.wallet.addByPrivateKey(GENESIS_PRIVATE_KEY);
-
-let globalStakingContractInfo;
-let globalToken0ContractInfo;
-let globalToken1ContractInfo;
-let globalToken2ContractInfo;
 
 let globalStakingContractAddress;
 let globalToken0ContractAddress;
@@ -69,119 +63,122 @@ beforeAll(async () => {
         if (!tx.getReceipt()!.success) {
           throw new Error();
         }
-      }
-      globalTestAccounts = accounts;
-      console.table({
-        GENESIS_PRIVATE_KEY,
-        OWNER: getTestAddr(OWNER),
-        USER: getTestAddr(USER),
-      });
+    }
+    globalTestAccounts = accounts;
 
-    const asyncFns = await [
-        CONTRACTS.staking_contract.path,
-        CONTRACTS.token0.path,
-        CONTRACTS.token1.path,
-        CONTRACTS.token2.path,
-    ].map(async (path) => useContractInfo(await getContractInfo(path, { container: CONTAINER })));
+    console.table({
+      GENESIS_PRIVATE_KEY,
+      OWNER: getTestAddr(OWNER),
+      USER: getTestAddr(USER),
+    });
 
-    [
-      globalStakingContractInfo,
-      globalToken0ContractInfo,
-      globalToken1ContractInfo,
-      globalToken2ContractInfo
-    ] = await Promise.all(asyncFns);
+    const init0 = getJSONParams({
+      _scilla_version: ["Uint32", 0],
+      contract_owner: ["ByStr20", getTestAddr(OWNER)],
+      name: ["String", CONTRACTS.token0.name],
+      symbol: ["String", CONTRACTS.token0.symbol],
+      decimals: ["Uint32", CONTRACTS.token0.decimals],
+      init_supply: ["Uint128", CONTRACTS.token0.init_supply],
+    });
 
-    let init0 = globalToken0ContractInfo.getInitParams(
-      getTestAddr(OWNER),
-      CONTRACTS.token0.name,
-      CONTRACTS.token0.symbol,
-      CONTRACTS.token0.decimals,
-      CONTRACTS.token0.init_supply
-    );
     [, contract] = await zilliqa.contracts
       .new(fs.readFileSync(CONTRACTS.token0.path).toString(), init0)
       .deploy(TX_PARAMS, 33, 1000, true);
       globalToken0ContractAddress = contract.address;
 
-    let init1 = globalToken1ContractInfo.getInitParams(
-      getTestAddr(OWNER),
-      CONTRACTS.token1.name,
-      CONTRACTS.token1.symbol,
-      CONTRACTS.token1.decimals,
-      CONTRACTS.token1.init_supply
-    );
+    const init1 = getJSONParams({
+      _scilla_version: ["Uint32", 0],
+      contract_owner: ["ByStr20", getTestAddr(OWNER)],
+      name: ["String", CONTRACTS.token1.name],
+      symbol: ["String", CONTRACTS.token1.symbol],
+      decimals: ["Uint32", CONTRACTS.token1.decimals],
+      init_supply: ["Uint128", CONTRACTS.token1.init_supply],
+    });
+
     [, contract] = await zilliqa.contracts
-      .new(fs.readFileSync(CONTRACTS.token0.path).toString(), init1)
+      .new(fs.readFileSync(CONTRACTS.token1.path).toString(), init1)
       .deploy(TX_PARAMS, 33, 1000, true);
-      globalToken1ContractAddress = contract.address;
+    globalToken1ContractAddress = contract.address;
 
-    let init2 = globalToken2ContractInfo.getInitParams(
-      getTestAddr(OWNER),
-      CONTRACTS.token2.name,
-      CONTRACTS.token2.symbol,
-      CONTRACTS.token2.decimals,
-      CONTRACTS.token2.init_supply
-    );
+
+    const init2 = getJSONParams({
+      _scilla_version: ["Uint32", 0],
+      contract_owner: ["ByStr20", getTestAddr(OWNER)],
+      name: ["String", CONTRACTS.token2.name],
+      symbol: ["String", CONTRACTS.token2.symbol],
+      decimals: ["Uint32", CONTRACTS.token2.decimals],
+      init_supply: ["Uint128", CONTRACTS.token2.init_supply],
+    });
+
     [, contract] = await zilliqa.contracts
-      .new(fs.readFileSync(CONTRACTS.token0.path).toString(), init2)
+      .new(fs.readFileSync(CONTRACTS.token2.path).toString(), init2)
       .deploy(TX_PARAMS, 33, 1000, true);
-      globalToken2ContractAddress = contract.address;
+    globalToken2ContractAddress = contract.address;
 
+    const init3 = getJSONParams({
+      _scilla_version: ["Uint32", 0],
+      init_contract_owner: ["ByStr20", getTestAddr(OWNER)],
+      init_staking_token_address: ["ByStr20", globalToken0ContractAddress],
+      blocks_per_cycle: ["Uint256", "10"],
+    });
 
-    let init3 = globalStakingContractInfo.getInitParams(
-      getTestAddr(OWNER),
-      globalToken0ContractAddress,
-      "10"
-    );
     [, contract] = await zilliqa.contracts
-    .new(fs.readFileSync(CONTRACTS.staking_contract.path).toString(), init3)
-    .deploy(TX_PARAMS, 33, 1000, true);
-    globalStakingContractAddress = contract.address;
+      .new(fs.readFileSync(CONTRACTS.staking_contract.path).toString(), init3)
+      .deploy(TX_PARAMS, 33, 1000, true);
+      globalStakingContractAddress = contract.address;
 
     console.table({
-        token0: globalToken0ContractAddress,
-        token1: globalToken1ContractAddress,
-        token2: globalToken2ContractAddress,
-        staking: globalStakingContractAddress
+      token0: globalToken0ContractAddress,
+      token1: globalToken1ContractAddress,
+      token2: globalToken2ContractAddress,
+      staking: globalStakingContractAddress
     });
 
     zilliqa.wallet.setDefault(getTestAddr(OWNER));
-    const tx0 = await globalToken0ContractInfo.callGetter(
-      zilliqa.contracts.at(globalToken0ContractAddress),
-      TX_PARAMS
-    )(
-        "IncreaseAllowance",
-        globalStakingContractAddress,
-        Number(100000000000000).toString()
+    const tx0: any = await zilliqa.contracts
+        .at(globalToken0ContractAddress)
+        .call(
+          "IncreaseAllowance",
+          getJSONParams({
+            spender: ["ByStr20", globalStakingContractAddress],
+            amount: ["Uint128", Number(100000000000000).toString()]
+          }),
+          TX_PARAMS
     );
     if (!tx0.receipt.success) {
       throw new Error();
     }
     console.log(tx0.receipt);
 
-    const tx1 = await globalToken1ContractInfo.callGetter(
-      zilliqa.contracts.at(globalToken1ContractAddress),
-      TX_PARAMS
-    )(
-      "IncreaseAllowance",
-      globalStakingContractAddress,
-      Number(100000000000000).toString()
-      );
+    const tx1: any = await zilliqa.contracts
+      .at(globalToken1ContractAddress)
+      .call(
+        "IncreaseAllowance",
+        getJSONParams({
+          spender: ["ByStr20", globalStakingContractAddress],
+          amount: ["Uint128", Number(100000000000000).toString()]
+        }),
+        TX_PARAMS
+    );
     if (!tx1.receipt.success) {
       throw new Error();
     }
+    console.log(tx1.receipt);
 
-    const tx2 = await globalToken2ContractInfo.callGetter(
-      zilliqa.contracts.at(globalToken2ContractAddress),
-      TX_PARAMS
-    )(
-      "IncreaseAllowance",
-      globalStakingContractAddress,
-      Number(100000000000000).toString()
+    const tx2: any = await zilliqa.contracts
+      .at(globalToken2ContractAddress)
+      .call(
+        "IncreaseAllowance",
+        getJSONParams({
+          spender: ["ByStr20", globalStakingContractAddress],
+          amount: ["Uint128", Number(100000000000000).toString()]
+        }),
+        TX_PARAMS
     );
     if (!tx2.receipt.success) {
       throw new Error();
     }
+    console.log(tx2.receipt);
 });
 
 
@@ -194,53 +191,54 @@ describe("staking contract", () => {
           beforeTransition: asyncNoop,
           error: undefined,
           getParams: () => ({}),
+          want: undefined
         },
         {
-            name: "deposit once",
-            transition: "deposit",
-            getSender: () => getTestAddr(OWNER),
-            getParams: () => ({
-              amount: "10"
-            }),
-            beforeTransition: asyncNoop,
-            error: undefined,
-            want: {
-              verifyState: (state) => {
-                return (
-                  JSON.stringify(state.total_stake_per_cycle) ===
-                    `{"1":"10"}` &&
-                  JSON.stringify(state.total_stake) === `"10"`
-                )
-              }
+          name: "deposit once",
+          transition: "deposit",
+          getSender: () => getTestAddr(OWNER),
+          getParams: () => ({
+            amount: ["Uint128", 10],
+          }),
+          beforeTransition: asyncNoop,
+          error: undefined,
+          want: {
+            verifyState: (state) => {
+              return (
+                JSON.stringify(state.total_stake_per_cycle) ===
+                  `{"1":"10"}` &&
+                JSON.stringify(state.total_stake) === `"10"`
+              )
             }
+          }
         },
         {
-            name: "deposit multiple times before next cycle",
-            transition: "deposit",
-            getSender: () => getTestAddr(OWNER),
-            getParams: () => ({
-              amount: "10"
-            }),
-            beforeTransition: async () => {
-              await increaseBNum(zilliqa, 5);
-            },
-            error: undefined,
-            want: {
-              verifyState: (state) => {
-                return (
-                  JSON.stringify(state.total_stake_per_cycle) ===
-                    `{"1":"20"}` &&
-                    JSON.stringify(state.total_stake) === `"20"`
-                )
-              }
+          name: "deposit multiple times before next cycle",
+          transition: "deposit",
+          getSender: () => getTestAddr(OWNER),
+          getParams: () => ({
+            amount: ["Uint128", 10],
+          }),
+          beforeTransition: async () => {
+            await increaseBNum(zilliqa, 5);
+          },
+          error: undefined,
+          want: {
+            verifyState: (state) => {
+              return (
+                JSON.stringify(state.total_stake_per_cycle) ===
+                  `{"1":"20"}` &&
+                  JSON.stringify(state.total_stake) === `"20"`
+              )
             }
+          }
         },
         {
           name: "deposit multiple times crross next cycle",
           transition: "deposit",
           getSender: () => getTestAddr(OWNER),
           getParams: () => ({
-            amount: "10"
+            amount: ["Uint128", 10],
           }),
           beforeTransition: async () => {
             await increaseBNum(zilliqa, 10);
@@ -261,7 +259,7 @@ describe("staking contract", () => {
           transition: "deposit",
           getSender: () => getTestAddr(OWNER),
           getParams: () => ({
-            amount: "10"
+            amount: ["Uint128", 10],
           }),
           beforeTransition: async () => {
             await increaseBNum(zilliqa, 33);
@@ -285,20 +283,20 @@ describe("staking contract", () => {
             await testCase.beforeTransition();
 
             zilliqa.wallet.setDefault(testCase.getSender());
-            const tx = await globalStakingContractInfo.callGetter(
-                zilliqa.contracts.at(globalStakingContractAddress),
+            const tx: any = await zilliqa.contracts
+              .at(globalStakingContractAddress)
+              .call(
+                testCase.transition,
+                getJSONParams(testCase.getParams()),
                 TX_PARAMS
-            )(testCase.transition, ...Object.values(testCase.getParams()));
+            );
             console.log("transaction id = ", tx.id);
             console.log(tx.receipt);
-            const state = await zilliqa.contracts
-                    .at(globalStakingContractAddress)
-                    .getState();
-            console.log(state);
-            if (!tx.receipt.success) {
-              throw new Error();
-            }
-            if (testCase.want !== undefined) {
+
+            if (testCase.want !== undefined && testCase.want.verifyState !== undefined) {
+              const state = await zilliqa.contracts
+              .at(globalStakingContractAddress)
+              .getState();
               expect(testCase.want.verifyState(state)).toBe(true);
             }
         });
