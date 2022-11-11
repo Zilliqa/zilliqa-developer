db375c9bdb606e53282782abeea79cc7746cd642
Troels F. Rønnow <troels@wonop.com>
2022-11-11 20:45:35 +0100

    Adding boilerplate

---
+++ products/wallet/src/App.tsx
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://zil-dev.cdn.prismic.io/zil-dev/f3b97b97-e98b-4767-9b24-9474b9c20a83_Asset+1.svg"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>This is the new</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
