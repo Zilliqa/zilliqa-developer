4f5355f0ec81de81193b6bb897982ff2a50df31d
Troels F. Rønnow <troels@wonop.com>
2022-11-14 09:48:52 +0100

    Updating config overrides

--- products/wallet/config-overrides.js
+++ products/wallet/config-overrides.js
@@ -2,7 +2,7 @@ const WorkerPlugin = require("worker-plugin");
 const webpack = require("webpack");
 
 module.exports = function override(config, env) {
-  // config.plugins.push(new WorkerPlugin());
+  //  config.plugins.push(new WorkerPlugin());
   /*
     config.resolve.fallback = {
       ...config.resolve.fallback,
@@ -27,31 +27,31 @@ module.exports = function override(config, env) {
       }),
     ];
   */
-  /*
-    config.resolve.fallback = {
-      url: require.resolve("url"),
-      assert: require.resolve("assert"),
-      crypto: require.resolve("crypto-browserify"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify/browser"),
-      buffer: require.resolve("buffer"),
-      stream: require.resolve("stream-browserify"),
-      string_decoder: require.resolve("string_decoder"),
-      events: require.resolve("events"),
-    };
 
-    config.plugins.push(new WorkerPlugin());
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        process: "process/browser",
-      })
-    );
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-      })
-    );
-    */
+  config.resolve.fallback = {
+    url: require.resolve("url"),
+    assert: require.resolve("assert"),
+    crypto: require.resolve("crypto-browserify"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify/browser"),
+    buffer: require.resolve("buffer"),
+    stream: require.resolve("stream-browserify"),
+    string_decoder: require.resolve("string_decoder"),
+    events: require.resolve("events"),
+  };
+
+  config.plugins.push(new WorkerPlugin());
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    })
+  );
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    })
+  );
+
   return config;
 };
