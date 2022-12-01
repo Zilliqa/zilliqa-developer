3b22c8d55d881d4bdb773f16b0265fcfcc8eef0a
Troels F. Rønnow <troels@wonop.com>
2022-12-01 11:04:01 +0100

    Creating patch workflow

--- .github/scripts/open-preview.py
+++ .github/scripts/open-preview.py
@@ -1,26 +1,28 @@
 import os
 import subprocess
 
+# Defining branch name
+print("Getting workspace status")
 workspace_status = subprocess.check_output(
     "python config/workspace-status.py", shell=True
 ).decode("utf-8")
 status = dict([tuple(x.split(" ", 1)) for x in workspace_status.strip().split("\n")])
-print(status)
 
 branch_id = "preview/developer-{}".format(status["STABLE_GIT_SHORT_HASH"])
 
+## Getting the devops repo
 subprocess.check_output(
     "git clone git@github.com:Zilliqa/devops.git .infra", shell=True
 )
+
+## Checking out the branch
+print("Switching branch")
 os.chdir(".infra")
+
 branch_output = subprocess.check_output(
     "git branch", stderr=subprocess.STDOUT, shell=True
 ).decode("utf-8")
 branches = [x.strip() for x in branch_output.split("\n") if x.strip() != ""]
-
-for branch in branches:
-    print("-", branch)
-
 
 if branch_id in branches:
     print("Checking preview branch out")
@@ -29,12 +31,33 @@ else:
     print("Creating branch")
     subprocess.check_output("git checkout -b {}".format(branch_id), shell=True)
 
-#          # Placeholder for doing real change
-#          echo "DO NOT MERGE" >> cd/applications/devportal/overlays/preview/kustomization.yaml
-#
-#          git add . -A
-#          git commit -m "Preparing preview for commit: ${{ github.sha }}
-#          git push
+# Creating the patch
+print("Creating patch")
+os.chdir("..")
+
+# TODO: Update this to one single patch
+subprocess.check_output(
+    "bazelisk build //products/developer-portal:cd_preview", shell=True
+)
+
+# Applying patch
+print("Applying patch")
+os.chdir(".infra")
+subprocess.check_output(
+    "tar xvf ../bazel-bin/products/developer-portal/cd_preview.tar", shell=True
+)
+
+
+# Pushing
+print("Pushing")
+# TODO: Check if anything updated
+subprocess.check_output("git add . -A", shell=True)
+subprocess.check_output(
+    'git commit -m "Preparing preview for commit: {}"'.format(
+        status["STABLE_GIT_SHORT_HASH"], shell=True
+    )
+)
+subprocess.check_output("git push", shell=True)
 
 
 exit(-1)
---
+++ config/expand_workspace_status.py
@@ -0,0 +1,47 @@
+import argparse
+import os
+import sys
+from string import Template
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Bake a git hash into a header & source."
+    )
+    parser.add_argument("--output", required=True, help="output file")
+
+    parser.add_argument("--template", required=True, help="template  file")
+    parser.add_argument("--true_value", required=True, help="value that denotes true")
+    parser.add_argument("--false_value", required=True, help="value that denotes false")
+
+    parser.add_argument(
+        "--volatile_file", required=True, help="file containing the volatile variables"
+    )
+    parser.add_argument(
+        "--stable_file", required=True, help="file containing the stable variables"
+    )
+
+    args = parser.parse_args()
+
+    with open(args.stable_file, "r") as fb:
+        stable_values = [
+            x.strip().split(" ", 1) for x in fb.readlines() if " " in x.strip()
+        ]
+
+    with open(args.volatile_file, "r") as fb:
+        volatile_values = [
+            x.strip().split(" ", 1) for x in fb.readlines() if " " in x.strip()
+        ]
+
+    values = dict(stable_values + volatile_values)
+
+    with open(args.template, "r") as fb:
+        template = Template(fb.read())
+
+    with open(args.output, "w") as f:
+        f.write(template.substitute(**values))
+
+
+if __name__ == "__main__":
+    main()
