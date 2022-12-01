3ed40533ea4100fe6ede81d020be6752730f3088
Troels F. Rønnow <troels@wonop.com>
2022-12-01 09:59:46 +0100

    Create branch

--- .github/scripts/open-preview.py
+++ .github/scripts/open-preview.py
@@ -1,12 +1,33 @@
 import os
 import subprocess
 
+workspace_status = subprocess.check_output(
+    "python config/workspace-status.py", shell=True
+)
+status = dict([tuple(x.split(" ", 1)) for x in workspace_status.strip().split("\n")])
+print(status)
+
+branch_id = "preview/developer-{}".format(status["STABLE_GIT_SHORT_HASH"])
+
 subprocess.check_output(
     "git clone git@github.com:Zilliqa/devops.git .infra", shell=True
 )
 os.chdir(".infra")
-branch_output = subprocess.checkout("git branch", stderr=subprocess.STDOUT, shell=True)
+branch_output = subprocess.check_output(
+    "git branch", stderr=subprocess.STDOUT, shell=True
+)
 branches = [x.strip() for x in branch_output.split("\n") if x.strip() != ""]
+
+for branch in branches:
+    print("-", branch)
+
+
+if branch_id in branches:
+    print("Checking preview branch out")
+    subprocess.check_output("git checkout {}".format(branch_id), shell=True)
+else:
+    print("Creating branch")
+    subprocess.check_output("git checkout -b {}".format(branch_id), shell=True)
 
 #          # Placeholder for doing real change
 #          echo "DO NOT MERGE" >> cd/applications/devportal/overlays/preview/kustomization.yaml
@@ -14,3 +35,6 @@ branches = [x.strip() for x in branch_output.split("\n") if x.strip() != ""]
 #          git add . -A
 #          git commit -m "Preparing preview for commit: ${{ github.sha }}
 #          git push
+
+
+exit(-1)
