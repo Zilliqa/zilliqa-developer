b3ea01004778ccfd71a423a25adcd7a05c3dc1f8
Troels F. Rønnow <troels@wonop.com>
2022-12-02 14:43:14 +0100

    Updating with versioning

--- cd/version/version.tpl.py
+++ cd/version/version.tpl.py
@@ -1,3 +1,3 @@
-stable_git_hash = "${STABLE_GIT_SHORT_HASH}"
-branch_id = "preview/developer-${STABLE_GIT_SHORT_HASH}"
+stable_git_hash = "${STABLE_HASH_VERSION}"
+branch_id = "preview/developer-${STABLE_HASH_VERSION}"
 commit_id = "${STABLE_GIT_COMMIT_HASH}"
--- config/workspace-status.py
+++ config/workspace-status.py
@@ -26,9 +26,10 @@ def main():
     version = get_version(".")
     version.update({"commit_hash": git_hash, "is_dirty": git_is_dirty})
     version["full_version"] = get_full_version(**version)
+    version["short_hash"] = git_hash[:7]
 
     print("STABLE_GIT_COMMIT_HASH {}".format(git_hash))
-    print("STABLE_GIT_SHORT_HASH {}".format(git_hash[:8]))
+    print("STABLE_GIT_SHORT_HASH {}".format(git_hash[:7]))
     print("STABLE_GIT_DIRTY {}".format("1" if git_is_dirty else "0"))
     print("STABLE_GIT_MAJOR {major}".format(**version))
     print("STABLE_GIT_MINOR {minor}".format(**version))
@@ -36,6 +37,7 @@ def main():
     print("STABLE_GIT_CHANNEL {channel}".format(**version))
     print("STABLE_GIT_PATCH {patch}".format(**version))
     print("STABLE_FULL_VERSION {full_version}".format(**version))
+    print("STABLE_HASH_VERSION {full_version}-{short_hash}".format(**version))
 
 
 def get_git_hash(path):
