eafed7c8849909fa966041963c34a00c3d27e953
Saayan Choudhury <saayan@zilliqa.com>
2023-04-20 16:14:47 +0800

    test script change

--- examples/evm-zil-examples/wallets/evm_zil_wallets/test/EVM_ZIL_Wallets.test.ts
+++ examples/evm-zil-examples/wallets/evm_zil_wallets/test/EVM_ZIL_Wallets.test.ts
@@ -1,107 +1,54 @@
+import { ScillaContract, initZilliqa, setAccount } from "hardhat-scilla-plugin";
+import { Value } from "@zilliqa-js/contract";
+import { assert, expect } from "chai";
 import hre, {web3} from "hardhat";
 import clc from "cli-color";
-import {bytes, toBech32Address, toChecksumAddress, units, Zilliqa} from "@zilliqa-js/zilliqa";
-import {getAddressFromPrivateKey} from "@zilliqa-js/crypto";
-const {BN, Long} = require("@zilliqa-js/util");
+import { loadZilliqaHardhatObject } from "hardhat-scilla-plugin/dist/src/ZilliqaHardhatObject";
+import * as zcrypto from "@zilliqa-js/crypto";
 
-async function main() {
-  const {provider} = hre.network;
+const newpk = zcrypto.schnorr.generatePrivateKey();
 
-  // Constants needed for zil transfer
-  const msgVersion = 1; // current msgVersion
-  const VERSION = bytes.pack(hre.getZilliqaChainId(), msgVersion);
+// these are all isolated server genesis accounts; if you're using devnet,
+// you'll need to find three accounts and put their private keys here.
+const adminpk =
+  "e53d1c3edaffc7a7bab5418eb836cf75819a82872b4a1a0f1c7fcf5c3e020b89";
+const userpk =
+  "d96e9eb5b782a80ea153c937fa83e5948485fbfc8b7e7c069d7b914dbc350aba";
 
-  const accounts: string[] = await provider.send("eth_accounts");
-  let balances = await Promise.all(
-    accounts.map((account: string) => provider.send("eth_getBalance", [account, "latest"]))
-  );
+const privateKeys = [adminpk, userpk];
+const network_url = "http://localhost:5555";
+const chain_id = 222; // 222;
+const setup = initZilliqa(network_url, chain_id, privateKeys);
 
-  // Print balances of eth accounts before
-  accounts.forEach((element, index) => {
-    console.log(
-      clc.bold("Account"),
-      clc.green(element),
-      clc.bold("Initial balance:"),
-      clc.greenBright(balances[index])
-    );
-  });
+const adminaddress = zcrypto.getAddressFromPrivateKey(adminpk).toLowerCase();
+const useraddress = zcrypto.getAddressFromPrivateKey(userpk).toLowerCase();
 
-  for (const element of hre.network["config"]["accounts"]) {
-    console.log("");
-    console.log("Starting transfer...");
+const adminpubkey = zcrypto.getPubKeyFromPrivateKey(adminpk);
+const userpubkey = zcrypto.getPubKeyFromPrivateKey(userpk);
 
-    // Get corresponding eth account
-    let ethAddr = web3.eth.accounts.privateKeyToAccount(element);
-    let ethAddrConverted = toChecksumAddress(ethAddr.address); // Zil checksum
+const adminwallet = zcrypto.getAddressFromPublicKey(adminpubkey)
+const userwallet = zcrypto.getAddressFromPublicKey(userpubkey)
+
+const adminwalletbech32 = zcrypto.toBech32Address(adminwallet)
+const userwalletbech32 = zcrypto.toBech32Address(userwallet)
+
+console.log("ZIL format: ")
+console.log(adminwallet)
+console.log(adminwalletbech32)
+console.log(adminwallet)
+console.log(userwalletbech32)
+
+console.log("EVM format: ")
+
+describe("EVM_ZIL_Wallets_Test", () => {
+
+  it("Check EVM balance", async () => {
+    let ethAddr = web3.eth.accounts.privateKeyToAccount(adminpk);
+    let ethAddrConverted = zcrypto.toChecksumAddress(ethAddr.address); // Zil checksum
     let initialAccountBal = await web3.eth.getBalance(ethAddr.address);
     console.log("Account to send to (zil checksum): ", ethAddrConverted);
     console.log("Account to send to, initial balance : ", initialAccountBal);
-
-    // Transfer half funds to this account
-    let zilliqa = new Zilliqa(hre.getNetworkUrl());
-    zilliqa.wallet.addByPrivateKey(element);
-    const address = getAddressFromPrivateKey(element);
-    console.log(`My ZIL account address is: ${address}`);
-
-    const res = await zilliqa.blockchain.getBalance(address);
-
-    if (res.error?.message) {
-      console.log("Error: ", res.error);
-      console.log("Skipping account with error");
-      continue;
-    }
-    const balance = res.result.balance;
-
-    console.log(`My ZIL account balance is: ${balance}`);
-
-    if (balance == 0) {
-      console.log("Skipping account with 0 balance");
-      continue;
-    }
-
-    const gasp = await web3.eth.getGasPrice();
-    const gasPrice = new BN(gasp);
-
-    const tx = await zilliqa.blockchain.createTransactionWithoutConfirm(
-      zilliqa.transactions.new(
-        {
-          version: VERSION,
-          toAddr: ethAddrConverted,
-          amount: new BN(balance).div(new BN(2)), // Sending an amount in Zil (1) and converting the amount to Qa
-          gasPrice: gasPrice, // Minimum gasPrice veries. Check the `GetMinimumGasPrice` on the blockchain
-          gasLimit: Long.fromNumber(2100)
-        },
-        false
-      )
-    );
-
-    // process confirm
-    console.log(`The transaction id is:`, tx.id);
-    const confirmedTxn = await tx.confirm(tx.id);
-
-    console.log(`The transaction status is:`);
-    console.log(confirmedTxn.receipt);
-
-    let finalBal = await web3.eth.getBalance(ethAddr.address);
-    console.log(`My new account balance is: ${finalBal}`);
-  }
-
-  balances = await Promise.all(accounts.map((account: string) => provider.send("eth_getBalance", [account, "latest"])));
-
-  // Print balances of eth accounts before
-  accounts.forEach((element, index) => {
-    console.log(
-      clc.bold("Account"),
-      clc.green(element),
-      clc.bold("Initial balance:"),
-      clc.greenBright(balances[index])
-    );
   });
-}
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  
+});
