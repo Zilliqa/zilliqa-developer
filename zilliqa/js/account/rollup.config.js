fe0cc3cac932fdb12f9349ceef1efea932b9ced7
Troels F. Rønnow <troels@wonop.com>
2023-05-25 17:46:40 +0200

    Fixing build

---
+++ zilliqa/js/account/rollup.config.js
@@ -0,0 +1,35 @@
+// rollup.config.js
+
+import commonjs from "rollup-plugin-commonjs";
+import builtins from "rollup-plugin-node-builtins";
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/umd/bundle.js",
+      format: "umd",
+      name: "@zilliqa-js/core",
+      sourcemap: !isProduction,
+    },
+    {
+      file: "dist/esm/bundle.js",
+      format: "esm",
+      sourcemap: !isProduction,
+    },
+  ],
+  plugins: [
+    typescript(),
+    resolve(),
+    commonjs(),
+    builtins(),
+    json(),
+    isProduction && terser(),
+  ],
+};
