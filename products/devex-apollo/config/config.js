2d10ee7c44bdf2f7bac91c50e0c6d752d9f8193f
Pablo Suarez <pablo@zilliqa.com>
2023-08-14 22:36:50 +0400

    feat: DEVOPS-903 devex-apollo gcp migration

--- products/devex-apollo/config/config.js
+++ products/devex-apollo/config/config.js
@@ -19,12 +19,12 @@ if (process.env.NODE_ENV === "dev") {
     console.log("Missing CA .pem in root");
   }
 
-  const documentDbConf =
-    "?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+  // const documentDbConf =
+  //   "?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 
   config = {
     database: process.env.DOCUMENTDB_DB,
-    dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/${documentDbConf}`,
+    dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/${process.env.DOCUMENTDB_DB}`,
     mongooseOpts: {
       useUnifiedTopology: true,
       useNewUrlParser: true,
