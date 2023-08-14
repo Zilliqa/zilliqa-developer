768af102288b64d36d9c92151d73af533eda4e83
Pablo Suarez <pablo@zilliqa.com>
2023-08-14 22:47:16 +0400

    feat: DEVOPS-903 devex-apollo gcp migration

--- products/devex-apollo/config/config.js
+++ products/devex-apollo/config/config.js
@@ -12,24 +12,13 @@ if (process.env.NODE_ENV === "dev") {
     },
   };
 } else {
-  let CA;
-  try {
-    CA = fs.readFileSync("./rds-combined-ca-bundle.pem");
-  } catch (e) {
-    console.log("Missing CA .pem in root");
-  }
-
-  // const documentDbConf =
-  //   "?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
-
   config = {
     database: process.env.DOCUMENTDB_DB,
-    dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/${process.env.DOCUMENTDB_DB}`,
+    dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/${process.env.DOCUMENTDB_DB}?authSource=${process.env.DOCUMENTDB_DB}`,
     mongooseOpts: {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
-      sslCA: [CA],
     },
   };
 }
