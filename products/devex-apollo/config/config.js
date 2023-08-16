31c4920dc4ed6ec3b24a54e1041711d110c4a054
Pablo Suarez <pablo@zilliqa.com>
2023-08-16 12:10:00 +0400

    feat: DEVOPS-903 devex-apollo gcp migration

--- products/devex-apollo/config/config.js
+++ products/devex-apollo/config/config.js
@@ -1,3 +1,5 @@
+//import fs from "fs";
+
 let config;
 
 if (process.env.NODE_ENV === "dev") {
@@ -10,13 +12,25 @@ if (process.env.NODE_ENV === "dev") {
     },
   };
 } else {
+  //  let CA;
+  //  try {
+  //    CA = fs.readFileSync("./rds-combined-ca-bundle.pem");
+  //  } catch (e) {
+  //    console.log("Missing CA .pem in root");
+  //  }
+
+  const documentDbConf =
+    "?authSource=" + process.env.DOCUMENTDB_DB + "&replicaSet=rs0";
+  //    "?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+
   config = {
     database: process.env.DOCUMENTDB_DB,
-    dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/?authSource=${process.env.DOCUMENTDB_DB}&replicaSet=rs0`,
+    dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/${documentDbConf}`,
     mongooseOpts: {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
+      //      sslCA: [CA],
     },
   };
 }
