d01f60a8a57a15a942c8f8950ff2038ad5c26f13
Ubuntu <ubuntu@ip-172-31-30-156.us-west-2.compute.internal>
2023-08-16 12:33:10 +0000

    devex-apollo local files

--- products/devex-apollo/config/config.js
+++ products/devex-apollo/config/config.js
@@ -1,11 +1,8 @@
-//import fs from "fs";
-
 let config;
 
 if (process.env.NODE_ENV === "dev") {
   config = {
     dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/${process.env.DOCUMENTDB_DB}?authSource=${process.env.DOCUMENTDB_DB}&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false`,
-    //    dbUrl: `mongodb://${process.env.DOCUMENTDB_USER}:${process.env.DOCUMENTDB_PASSWORD}@${process.env.DOCUMENTDB_HOST}:${process.env.DOCUMENTDB_PORT}/${process.env.DOCUMENTDB_DB}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false`,
     mongooseOpts: {
       useUnifiedTopology: true,
       useNewUrlParser: true,
@@ -13,16 +10,8 @@ if (process.env.NODE_ENV === "dev") {
     },
   };
 } else {
-  //  let CA;
-  //  try {
-  //    CA = fs.readFileSync("./rds-combined-ca-bundle.pem");
-  //  } catch (e) {
-  //    console.log("Missing CA .pem in root");
-  //  }
-
   const documentDbConf =
     "?authSource=" + process.env.DOCUMENTDB_DB + "&replicaSet=rs0";
-  //    "?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 
   config = {
     database: process.env.DOCUMENTDB_DB,
@@ -31,7 +20,6 @@ if (process.env.NODE_ENV === "dev") {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
-      //      sslCA: [CA],
     },
   };
 }
