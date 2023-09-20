9cd589bf175dd92a8770ad4b8c6b621c9880cd76
Troels F. Rønnow <troels@wonop.com>
2023-09-20 15:47:42 +0200

    Improving UI

--- products/bluebell/playground/src/app.rs
+++ products/bluebell/playground/src/app.rs
@@ -1,4 +1,5 @@
 use crate::logger::LoggerView;
+use crate::state::ExecutionStatus;
 
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -281,157 +282,166 @@ pub fn app() -> Html {
     let show_console = true;
 
     html! {
-        <AppLayout key={*current_view} menu={menu} on_select_view={set_current_view} view={*current_view}>
-        {
-            if let Some((is_error, exit_message)) = exit_message {
-                html! {
-                    <div class="z-10 pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
-                      <div class={
-                            if *is_error {
-                                "pointer-events-auto flex items-center justify-between gap-x-6 bg-red-600 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5"
-                            } else {
-                                "pointer-events-auto flex items-center justify-between gap-x-6 bg-yellow-600 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5"
-                            }
-                        }>
-                        <p class="text-sm leading-6 text-white">
-                          <a href="#">
-                            <strong class="font-semibold">{"Execution failed"}</strong>
-                            <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>
-                            {exit_message}<span aria-hidden="true"></span>
-                          </a>
-                        </p>
-                        <button type="button" class="-m-1.5 flex-none p-1.5">
-                          <span class="sr-only">{"Dismiss"}</span>
-                          <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-                          </svg>
-                        </button>
-                      </div>
-                    </div>
-                }
-
-            } else {
-                 html!{}
-            }
-        }
-                { if *current_view == 0 {
-
+            <AppLayout key={*current_view} menu={menu} on_select_view={set_current_view} view={*current_view}>
+            {
+                if let Some((error_code, exit_message)) = exit_message {
                     html! {
-                        <div class="h-full w-full flex flex-col pl-10 bg-black">
-                            <div class="editor-container flex-1 w-full bg-black  text-white text-left font-mono">
-                                <div class="w-full flex items-center justify-center py-2">
-                                    <Dropdown items={EXAMPLES.iter().map(|item| item.0.to_string()).collect::<Vec<_>>()}    on_item_click={|i:usize| {
-                                        let value: String = EXAMPLES[i].1.to_string().clone();
-                                        Dispatch::<State>::new().reduce_mut(move | s| {
-                                            s.program_counter = 0;
-                                            s.executable = None;
-                                            s.observable_machine = None;
-                                            s.pc_to_position = HashMap::new();
-                                            s.current_position = None;
-                                            s.source_code = value
-                                        })
-                                    }}    />
-                                </div>
-                                {
-                                    if let Some(p) = state.current_position {
-                                        let (_, _, line, _) = p;
-                                        let pos = if line < usize::MAX/2 {
-                                            line_to_pixel_offset(line+1)
-                                        }
-                                        else
-                                        {
-                                            0
-                                        };
-                                        html! {
-                                        <div class="bg-blue-600 w-full h-6 absolute" style={format!("top: {}px", pos)}>
-                                        </div>
-                                        }
-                                    } else {
-                                        html! {}
-                                    }
+                        <div class="z-10 pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
+                          <div class={
+                                match *error_code {
+                                    ExecutionStatus::Succeeded => "pointer-events-auto flex items-center justify-between gap-x-6 bg-green-600 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5",
+                                    ExecutionStatus::Paused => "pointer-events-auto flex items-center justify-between gap-x-6 bg-yellow-600 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5",
+                                    ExecutionStatus::Failed => "pointer-events-auto flex items-center justify-between gap-x-6 bg-red-600 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5"
                                 }
-                                <pre class="highlighted-code h-full w-full flex flex-col items-stretch hidden">
-                                    <code class="language-scilla h-full w-full"> /* TODO: Fix highligther */
-                                    {source_code.clone()}
-                                    </code>
-                                </pre>
-                                <textarea
-                                    class="overlay-textarea text-white outline-0 text-left font-mono h-full w-full focus:none"
-                                    value={source_code}
-                                    oninput={handle_source_code_change}
-                                />
+                            }>
+                            <p class="text-sm leading-6 text-white">
+                              <a href="#">
+                                <strong class="font-semibold">
+    {
+                                match *error_code {
+                                    ExecutionStatus::Succeeded => "Execution suceeded",
+                                    ExecutionStatus::Paused => "Execution paused",
+                                    ExecutionStatus::Failed => "Execution failed"
+                                }
+                            }
 
-                                { for error_map.iter().map(|(line, error)| {
-                                    html! {
-                                        <div class="error-annotation" style={format!("top: {}px", line_to_pixel_offset(*line))}>
-                                            { error }
-                                        </div>
-                                    }
-                                })}
-                            </div>
+                                </strong>
+                                <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>
+                                {exit_message}<span aria-hidden="true"></span>
+                              </a>
+                            </p>
+                            <button type="button" class="-m-1.5 flex-none p-1.5">
+                              <span class="sr-only">{"Dismiss"}</span>
+                              <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+                    }
 
+                } else {
+                     html!{}
+                }
+            }
+                    { if *current_view == 0 {
 
-                                <LoggerView />
-
-
-
-                            <div class="absolute -right-8 top-10 z-20 flex justify-center items-center">
-                                <button onclick={compile_button_click.clone()} class={
-                                    if state.executable.is_none() {
-                                        "ease-in-out delay-150 transition transition-opacity opacity-1 h-16 w-16 bg-indigo-600 text-lg text-white px-2 py-2 rounded-full shadow-sm hover:bg-indigo-700 focus:bg-indigo-800 focus:outline-none focus:ring focus:ring-indigo-200 active:bg-indigo-800 transition duration-150 flex items-center justify-center"
-                                    } else {
-                                        "ease-in-out delay-150 transition transition-opacity opacity-0 h-16 w-16 bg-indigo-600 text-lg text-white px-2 py-2 rounded-full shadow-sm hover:bg-indigo-700 focus:bg-indigo-800 focus:outline-none focus:ring focus:ring-indigo-200 active:bg-indigo-800 transition duration-150 flex items-center justify-center"
-                                    }
-                                }>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class={
-                                        if is_compiling {
-                                            "w-6 h-6 animate-spin"
+                        html! {
+                            <div class="h-full w-full flex flex-col pl-10 bg-black">
+                                <div class="editor-container flex-1 w-full bg-black  text-white text-left font-mono">
+                                    <div class="w-full flex items-center justify-center py-2">
+                                        <Dropdown items={EXAMPLES.iter().map(|item| item.0.to_string()).collect::<Vec<_>>()}    on_item_click={|i:usize| {
+                                            let value: String = EXAMPLES[i].1.to_string().clone();
+                                            Dispatch::<State>::new().reduce_mut(move | s| {
+                                                s.program_counter = 0;
+                                                s.executable = None;
+                                                s.observable_machine = None;
+                                                s.pc_to_position = HashMap::new();
+                                                s.current_position = None;
+                                                s.source_code = value
+                                            })
+                                        }}    />
+                                    </div>
+                                    {
+                                        if let Some(p) = state.current_position {
+                                            let (_, _, line, _) = p;
+                                            let pos = if line < usize::MAX/2 {
+                                                line_to_pixel_offset(line+1)
+                                            }
+                                            else
+                                            {
+                                                0
+                                            };
+                                            html! {
+                                            <div class="bg-blue-600 w-full h-6 absolute" style={format!("top: {}px", pos)}>
+                                            </div>
+                                            }
                                         } else {
-                                            "w-6 h-6"
+                                            html! {}
+                                        }
+                                    }
+                                    <pre class="highlighted-code h-full w-full flex flex-col items-stretch hidden">
+                                        <code class="language-scilla h-full w-full"> /* TODO: Fix highligther */
+                                        {source_code.clone()}
+                                        </code>
+                                    </pre>
+                                    <textarea
+                                        class="overlay-textarea text-white outline-0 text-left font-mono h-full w-full focus:none"
+                                        value={source_code}
+                                        oninput={handle_source_code_change}
+                                    />
+
+                                    { for error_map.iter().map(|(line, error)| {
+                                        html! {
+                                            <div class="error-annotation" style={format!("top: {}px", line_to_pixel_offset(*line))}>
+                                                { error }
+                                            </div>
+                                        }
+                                    })}
+                                </div>
+
+
+                                    <LoggerView />
+
+
+
+                                <div class="absolute -right-8 top-10 z-20 flex justify-center items-center">
+                                    <button onclick={compile_button_click.clone()} class={
+                                        if state.executable.is_none() {
+                                            "ease-in-out delay-150 transition transition-opacity opacity-1 h-16 w-16 bg-indigo-600 text-lg text-white px-2 py-2 rounded-full shadow-sm hover:bg-indigo-700 focus:bg-indigo-800 focus:outline-none focus:ring focus:ring-indigo-200 active:bg-indigo-800 transition duration-150 flex items-center justify-center"
+                                        } else {
+                                            "ease-in-out delay-150 transition transition-opacity opacity-0 h-16 w-16 bg-indigo-600 text-lg text-white px-2 py-2 rounded-full shadow-sm hover:bg-indigo-700 focus:bg-indigo-800 focus:outline-none focus:ring focus:ring-indigo-200 active:bg-indigo-800 transition duration-150 flex items-center justify-center"
                                         }
                                     }>
-                                      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
-                                    </svg>
-                                </button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class={
+                                            if is_compiling {
+                                                "w-6 h-6 animate-spin"
+                                            } else {
+                                                "w-6 h-6"
+                                            }
+                                        }>
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    }
+                        }
 
-                } else if *current_view == 1 {
-                            html! {
-                                <MachineView  />
-                            }
-                } else  if *current_view == 2 {
+                    } else if *current_view == 1 {
+                                html! {
+                                    <MachineView  />
+                                }
+                    } else  if *current_view == 2 {
 
-                    let (before, byte_at_pc, after) = if program_counter * 2 < bytecode_hex.len() {
-                        let (start, rest) = bytecode_hex.split_at(program_counter * 2);
-                        let (byte, end) = rest.split_at(2);
-                        (start, byte, end)
+                        let (before, byte_at_pc, after) = if program_counter * 2 < bytecode_hex.len() {
+                            let (start, rest) = bytecode_hex.split_at(program_counter * 2);
+                            let (byte, end) = rest.split_at(2);
+                            (start, byte, end)
+                        } else {
+                            (&bytecode_hex[..], "", "")
+                        };
+
+
+                        html! {
+                            <div class="h-full w-full pl-10 bg-black">
+                                <div
+                                    id="bytecode"
+                                    class="text-left break-words w-full h-screen bg-black text-white font-mono focus:none outline:none"
+                                >
+                                    { before }
+                                    <span class="bg-yellow-600 text-black px-1">{ byte_at_pc }</span>
+                                    { after }
+                                </div>
+                            </div>
+                        }
                     } else {
-                        (&bytecode_hex[..], "", "")
-                    };
-
-
-                    html! {
-                        <div class="h-full w-full pl-10 bg-black">
-                            <div
-                                id="bytecode"
-                                class="text-left break-words w-full h-screen bg-black text-white font-mono focus:none outline:none"
-                            >
-                                { before }
-                                <span class="bg-yellow-600 text-black px-1">{ byte_at_pc }</span>
-                                { after }
-                            </div>
-                        </div>
-                    }
-                } else {
-                    html! {
-                        <div>{"Undefined view"}</div>
+                        html! {
+                            <div>{"Undefined view"}</div>
+                        }
                     }
                 }
-            }
 
 
-        </AppLayout>
-    }
+            </AppLayout>
+        }
 }
