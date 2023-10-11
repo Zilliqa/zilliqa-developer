e408d4977df48072137ea931ee1c45df2432dfd2
Bruno Wu <wubruno@outlook.com>
2023-10-11 17:50:42 +0100

    ZIL-5409: Fix linting issue on PDT (#219)
    * Fix linting issue on PDT
    
    * Fix format issues
    
    * Markdown link lint support 405 for RPCs
    
    * Ignore links that return 403 on actions
    
    * Fix 403 errors

--- products/pdt/pdtdb/src/tracked.rs
+++ products/pdt/pdtdb/src/tracked.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use std::ops::Range;
 
-use anyhow::{Result};
+use anyhow::Result;
 use async_trait::async_trait;
 
 use crate::zqproj::{BlockInsertable, Inserter, InsertionErrors, PSQLInsertable};
