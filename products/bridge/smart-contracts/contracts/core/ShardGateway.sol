ca5b78e0d12118aa9fcf9dcccbc374211e05f04c
Bruno Wu <wubruno@outlook.com>
2023-12-11 16:23:20 +0000

    DispatchReplayChecker tests and separate ShardDispatcher

--- products/bridge/smart-contracts/contracts/core/ShardGateway.sol
+++ products/bridge/smart-contracts/contracts/core/ShardGateway.sol
@@ -2,29 +2,6 @@
 pragma solidity ^0.8.20;
 
 import {Relayer} from "contracts/core/Relayer.sol";
-import {DispatchReplayChecker} from "contracts/core/DispatchReplayChecker.sol";
+import {ShardDispatcher} from "contracts/core/ShardDispatcher.sol";
 
-interface IShardGatewayErrors {
-    error unauthorized();
-}
-
-interface IShardGatewayEvents {
-    event Dispatched(uint indexed sourceShardId, uint indexed nonce);
-}
-
-contract ShardGateway is IShardGatewayEvents, Relayer, DispatchReplayChecker {
-    modifier onlySelf() {
-        if (msg.sender != address(this)) {
-            revert IShardGatewayErrors.unauthorized();
-        }
-        _;
-    }
-
-    function replayDispatchCheck(
-        uint sourceShardId,
-        uint nonce
-    ) external onlySelf {
-        _replayDispatchCheck(sourceShardId, nonce);
-        emit Dispatched(sourceShardId, nonce);
-    }
-}
+contract ShardGateway is Relayer, ShardDispatcher {}
--- products/bridge/smart-contracts/foundry/test/Helpers.sol
+++ products/bridge/smart-contracts/foundry/test/Helpers.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity 0.8.20;
 
-import "contracts/core/ValidatorManager.sol";
-import "foundry/test/Tester.sol";
+import {ValidatorManager} from "contracts/core/ValidatorManager.sol";
+import {Tester, Vm} from "foundry/test/Tester.sol";
 
 contract TransferReentrancyTester {
     address target;
