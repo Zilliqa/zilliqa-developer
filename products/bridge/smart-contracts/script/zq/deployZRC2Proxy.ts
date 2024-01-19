9cc6d6cb56869feadb27f8ac8d198155f565e510
Bruno Wu <wubruno@outlook.com>
2024-01-19 16:34:55 +0000

    Update the ZRC2Proxy contract

--- products/bridge/smart-contracts/script/zq/deployZRC2Proxy.ts
+++ products/bridge/smart-contracts/script/zq/deployZRC2Proxy.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
   const address = "0xa3eafd5021f6b9c36fd02ed58aa1d015f2238791";
-  const ZRC2Proxy = await ethers.deployContract("EIP20ZRC2Proxy", [address]);
+  const ZRC2Proxy = await ethers.deployContract("ZRC2Proxy", [address]);
 
   await ZRC2Proxy.waitForDeployment();
 
