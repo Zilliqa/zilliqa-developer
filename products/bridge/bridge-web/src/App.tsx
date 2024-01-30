36011e48d492e5a0c89ab924c21bb307c0d41aa6
Bruno Wu <wubruno@outlook.com>
2024-01-30 15:22:09 +0000

    Update app.js

--- products/bridge/bridge-web/src/App.tsx
+++ products/bridge/bridge-web/src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
@@ -9,14 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
