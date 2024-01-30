2bdde9dce811cfac569dd092f210d499049efdf1
Bruno Wu <wubruno@outlook.com>
2024-01-30 11:48:41 +0000

    Add tailwind css

--- products/bridge/bridge-web/src/App.tsx
+++ products/bridge/bridge-web/src/App.tsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className="text-4xl font-bold underline">Hello world!</h1>
     </>
   );
 }
