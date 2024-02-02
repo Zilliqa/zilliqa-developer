1ba0c10d2cd2c6a1d5b04337e895c53bc249ba62
Bruno Wu <wubruno@outlook.com>
2024-02-02 12:59:39 +0000

    ZIL-5550: Bridge tidy up frontend (#382)
    * ZIL-5550: Bridge Tidy up Frontend
    
    * Updated corners
    
    * Fix failing builds
    
    * Update ci

--- products/bridge/bridge-web/src/App.tsx
+++ products/bridge/bridge-web/src/App.tsx
@@ -1,7 +1,14 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import zilliqa from "./assets/zilliqa.png";
+import { fromBech32Address, toBech32Address } from "@zilliqa-js/crypto";
+import { validation } from "@zilliqa-js/util";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRight,
+  faArrowUpRightFromSquare,
+  faChevronDown,
+  faRepeat,
+} from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import { Chains, TokenConfig, chainConfigs } from "./config/config";
 import {
@@ -51,6 +58,7 @@ function App() {
     args: account ? [account.address!] : undefined,
     address: token.address,
     enabled: !!account.address,
+    watch: true,
   });
 
   useEffect(() => {
@@ -69,7 +77,16 @@ function App() {
     address: token.address,
     args: [account.address!, fromChainConfig.tokenManagerAddress],
     enabled: !!account.address,
+    watch: true,
   });
+
+  const hasEnoughAllowance =
+    decimals && allowance
+      ? allowance > parseUnits(amount.toString(), decimals)
+      : false;
+  const hasValidAddress = recipient
+    ? validation.isBech32(recipient) || validation.isAddress(recipient)
+    : false;
 
   const { config: transferConfig } = usePrepareContractWrite({
     address: fromChainConfig.tokenManagerAddress,
@@ -90,16 +107,15 @@ function App() {
     mode: "prepared",
     request: {
       address: fromChainConfig.tokenManagerAddress,
+      chain: fromChainConfig.wagmiChain,
+      account: account.address!,
       abi: MintAndBurnTokenManagerAbi,
-      args:
-        toChainConfig && recipient && amount && decimals
-          ? [
-              token.address,
-              BigInt(toChainConfig.chainId),
-              recipient as `0x${string}`,
-              parseUnits(amount.toString(), decimals),
-            ]
-          : undefined,
+      args: [
+        token.address,
+        BigInt(toChainConfig.chainId),
+        recipient as `0x${string}`,
+        parseUnits(amount.toString(), decimals!),
+      ],
       functionName: "transfer",
       gas: 600_000n,
       type: "legacy",
@@ -115,7 +131,7 @@ function App() {
       abi: erc20ABI,
       args: [
         fromChainConfig.tokenManagerAddress,
-        parseUnits(amount.toString(), decimals || 0),
+        parseUnits(amount.toString(), decimals ?? 0),
       ],
       functionName: "approve",
       type: "legacy",
@@ -123,6 +139,10 @@ function App() {
 
   const { writeAsync: approve, isLoading: isLoadingApprove } =
     useContractWrite(approveConfig);
+
+  const canBridge =
+    !hasValidAddress ||
+    (fromChainConfig.isZilliqa ? false : !amount || isLoadingBridge);
 
   return (
     <>
@@ -135,156 +155,214 @@ function App() {
             <ConnectButton />
           </div>
         </div>
+
         <div className="card min-h-96 bg-neutral shadow-xl">
           <div className="card-body">
             <div className="card-title">
-              <p className="text-4xl">Zilliqa Bridge</p>
+              <p className="text-4xl text-center tracking-wide">BRIDGE</p>
             </div>
 
-            <label>Network</label>
-            <div className="flex justify-between items-center gap-3">
-              <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn m-1 w-52">
-                  {fromChainConfig.name}
-                  <FontAwesomeIcon
-                    icon={faChevronDown}
-                    className="ml-auto"
-                    color="white"
-                  />
-                </div>
-
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-                >
-                  {Object.values(chainConfigs)
-                    .filter((config) => config.chain !== fromChainConfig.chain)
-                    .map(({ chain, name }) => (
-                      <li
-                        key={`from${chain}`}
-                        onClick={() => {
-                          if (chain === toChain) {
-                            setToChain(
-                              Object.values(chainConfigs).filter(
-                                (chains) => chains.chain !== chain
-                              )[0].chain
-                            );
-                          }
-                          setFromChain(chain);
-                          blur();
-                        }}
-                      >
-                        <a>{name}</a>
-                      </li>
-                    ))}
-                </ul>
+            <div className="form-control">
+              <div className="label">
+                <span>Networks</span>
               </div>
-              <FontAwesomeIcon icon={faArrowRight} color="white" />
-              <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn m-1 w-52">
-                  {toChainConfig.name}
-                  <FontAwesomeIcon
-                    icon={faChevronDown}
-                    color="white"
-                    className="ml-auto"
-                  />
-                </div>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-                >
-                  {Object.values(chainConfigs)
-                    .filter(({ chain }) => chain !== toChainConfig.chain)
-                    .map(({ chain, name }) => (
-                      <li
-                        key={`to${chain}`}
-                        onClick={() => {
-                          if (chain === fromChain) {
-                            setFromChain(
-                              Object.values(chainConfigs).filter(
-                                (chains) => chains.chain !== chain
-                              )[0].chain
-                            );
-                          }
-                          setToChain(chain);
-                          blur();
-                        }}
-                      >
-                        <a>{name}</a>
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            </div>
-
-            <label>Recipient Address</label>
-            <input
-              type="text"
-              placeholder="Zil Address"
-              className="input w-full"
-              value={recipient}
-              onChange={({ target }) => setRecipient(target.value)}
-            />
-
-            <label>Token</label>
-            <div className="flex flex-col">
               <div className="join">
-                <div className="indicator">
-                  <button className="btn join-item w-32">
-                    FPS
+                <div className="dropdown">
+                  <div tabIndex={0} role="button" className="btn w-52">
+                    {fromChainConfig.name}
                     <FontAwesomeIcon
                       icon={faChevronDown}
+                      className="ml-auto"
+                      color="white"
+                    />
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                  >
+                    {Object.values(chainConfigs)
+                      .filter(
+                        (config) => config.chain !== fromChainConfig.chain
+                      )
+                      .map(({ chain, name }) => (
+                        <li
+                          key={`from${chain}`}
+                          onClick={() => {
+                            if (chain === toChain) {
+                              setToChain(fromChain);
+                            }
+                            setFromChain(chain);
+                            blur();
+                          }}
+                        >
+                          <a>{name}</a>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+                <FontAwesomeIcon
+                  className="w-10 self-center"
+                  icon={faArrowRight}
+                  color="white"
+                />
+                <div className="dropdown">
+                  <div tabIndex={0} role="button" className="btn w-52">
+                    {toChainConfig.name}
+                    <FontAwesomeIcon
+                      icon={faChevronDown}
+                      color="white"
+                      className="ml-auto"
+                    />
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                  >
+                    {Object.values(chainConfigs)
+                      .filter(({ chain }) => chain !== toChainConfig.chain)
+                      .map(({ chain, name }) => (
+                        <li
+                          key={`to${chain}`}
+                          onClick={() => {
+                            if (chain === fromChain) {
+                              setFromChain(toChain);
+                            }
+                            setToChain(chain);
+                            blur();
+                          }}
+                        >
+                          <a>{name}</a>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="form-control">
+              <div className="label">
+                <span>Recipient</span>
+              </div>
+              <div className="join">
+                <div className="indicator">
+                  <button
+                    className="btn join-item"
+                    disabled={!hasValidAddress}
+                    onClick={() => {
+                      setRecipient((_recipient) => {
+                        if (!_recipient) {
+                          return _recipient;
+                        }
+                        if (validation.isBech32(_recipient)) {
+                          return fromBech32Address(_recipient!);
+                        }
+                        if (validation.isAddress(_recipient)) {
+                          return toBech32Address(_recipient);
+                        }
+                      });
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faRepeat}
                       color="white"
                       className="ml-auto"
                     />
                   </button>
                 </div>
                 <input
-                  className="input join-item input-bordered w-full text-right"
+                  className={`input join-item input-bordered w-full font-mono text-sm text-end ${
+                    !hasValidAddress && "input-warning"
+                  }`}
+                  placeholder="Address"
+                  value={recipient}
+                  onChange={({ target }) => setRecipient(target.value)}
+                />
+              </div>
+              {!hasValidAddress && (
+                <div className="label align-bottom place-content-end">
+                  <span className="label-text-alt text-warning">
+                    Invalid Address
+                  </span>
+                </div>
+              )}
+            </div>
+
+            <div className="form-control">
+              <div className="label">
+                <span>Token</span>
+                <span className="label-text-alt self-end">
+                  Balance:{" "}
+                  {balance && decimals ? formatUnits(balance, decimals) : null}
+                </span>
+              </div>
+              <div className="join">
+                <div className="indicator">
+                  <button
+                    onClick={() => window.open(token.blockExplorer, "_blank")}
+                    className="btn join-item w-32"
+                  >
+                    <p>{token.name}</p>
+                    <FontAwesomeIcon
+                      icon={faArrowUpRightFromSquare}
+                      color="white"
+                      className="ml-auto"
+                    />
+                  </button>
+                </div>
+                <input
+                  className={`input join-item input-bordered w-full text-right ${
+                    !hasEnoughAllowance && "input-warning"
+                  }`}
                   placeholder="Amount"
                   type="number"
                   onChange={({ target }) => setAmount(Number(target.value))}
                 />
               </div>
-              <div className="label align-bottom place-content-end">
-                <span className="label-text-alt">
-                  Balance:{" "}
-                  {balance && decimals ? formatUnits(balance, decimals) : null}
-                </span>
-              </div>
+              {!hasEnoughAllowance && (
+                <div className="label align-bottom place-content-end">
+                  <span className="label-text-alt text-warning">
+                    Insufficient allowance
+                  </span>
+                </div>
+              )}
             </div>
             <div className="card-actions mt-auto pt-4">
-              <button
-                className="btn w-5/6 mx-10 btn-outline text-primary-content"
-                disabled={
-                  !amount || !approve || isLoadingApprove || isErrorApprove
-                }
-                onClick={async () => {
-                  if (approve) {
-                    const res = await approve();
-                    console.log(res.hash);
+              {hasEnoughAllowance ? (
+                <button
+                  className="btn w-5/6 mx-10 btn-primary text-primary-content"
+                  disabled={canBridge}
+                  onClick={async () => {
+                    if (fromChainConfig.isZilliqa && bridgeZilliqa) {
+                      const tx = await bridgeZilliqa();
+                      console.log(tx.hash);
+                    } else if (bridge) {
+                      const res = await bridge();
+                      console.log(res.hash);
+                    }
+                  }}
+                >
+                  Bridge
+                </button>
+              ) : (
+                <button
+                  className="btn w-5/6 mx-10 btn-outline"
+                  disabled={
+                    !amount ||
+                    !approve ||
+                    isLoadingApprove ||
+                    isErrorApprove ||
+                    hasEnoughAllowance
                   }
-                }}
-              >
-                Approve
-              </button>
-              <button
-                className="btn w-5/6 mx-10 btn-primary text-primary-content"
-                disabled={
-                  fromChainConfig.isZilliqa ? false : !amount || isLoadingBridge
-                }
-                onClick={async () => {
-                  if (fromChainConfig.isZilliqa && bridgeZilliqa) {
-                    const tx = await bridgeZilliqa();
-                    console.log(tx.hash);
-                  } else if (bridge) {
-                    const res = await bridge();
-                    console.log(res.hash);
-                  }
-                }}
-              >
-                Bridge
-              </button>
+                  onClick={async () => {
+                    if (approve) {
+                      const res = await approve();
+                      console.log(res.hash);
+                    }
+                  }}
+                >
+                  Approve
+                </button>
+              )}
             </div>
           </div>
         </div>
--- products/bridge/bridge-web/src/config/config.ts
+++ products/bridge/bridge-web/src/config/config.ts
@@ -1,3 +1,6 @@
+import { Chain } from "viem";
+import { bscTestnet, zilliqaTestnet } from "viem/chains";
+
 export enum TokenManagerType {
   MintAndBurn,
   LockAndRelease,
@@ -9,12 +12,15 @@ export const chainConfigs: Record<Chains, ChainConfig> = {
   "bsc-testnet": {
     chain: "bsc-testnet",
     name: "BSC Testnet",
+    wagmiChain: bscTestnet,
     tokenManagerAddress: "0xA6D73210AF20a59832F264fbD991D2abf28401d0",
     tokenManagerType: TokenManagerType.MintAndBurn,
     tokens: [
       {
-        name: "FPS",
+        name: "TST",
         address: "0x5190e8b4Bbe8C3a732BAdB600b57fD42ACbB9F4B",
+        blockExplorer:
+          "https://testnet.bscscan.com/address/0x5190e8b4Bbe8C3a732BAdB600b57fD42ACbB9F4B",
       },
     ],
     chainId: 97,
@@ -25,10 +31,13 @@ export const chainConfigs: Record<Chains, ChainConfig> = {
     name: "Zilliqa Testnet",
     tokenManagerAddress: "0x1509988c41f02014aA59d455c6a0D67b5b50f129",
     tokenManagerType: TokenManagerType.LockAndRelease,
+    wagmiChain: zilliqaTestnet,
     tokens: [
       {
-        name: "FPS",
+        name: "TST",
         address: "0x8618d39a8276D931603c6Bc7306af6A53aD2F1F3",
+        blockExplorer:
+          "https://otterscan.testnet.zilliqa.com/address/0x8618d39a8276D931603c6Bc7306af6A53aD2F1F3",
       },
     ],
     chainId: 33101,
@@ -39,14 +48,16 @@ export const chainConfigs: Record<Chains, ChainConfig> = {
 export type ChainConfig = {
   name: string;
   chain: Chains;
+  wagmiChain: Chain;
   tokenManagerAddress: `0x${string}`;
   tokenManagerType: TokenManagerType;
   tokens: TokenConfig[];
   chainId: number;
-  isZilliqa: bool;
+  isZilliqa: boolean;
 };
 
 export type TokenConfig = {
   name: string;
   address: `0x${string}`;
+  blockExplorer: string;
 };
