905af588e82a0806e1a543e8d3e0445c91f46db2
Bruno Wu <wubruno@outlook.com>
2024-02-02 16:45:36 +0000

    ZIL-5550: Bridge Web fix approval gas

--- products/bridge/bridge-web/src/App.tsx
+++ products/bridge/bridge-web/src/App.tsx
@@ -163,6 +163,7 @@ function App() {
       parseUnits(amount.toString(), decimals ?? 0),
     ],
     functionName: "approve",
+    gas: fromChainConfig.isZilliqa ? 400_000n : undefined,
     type: fromChainConfig.isZilliqa ? "legacy" : "eip1559",
   });
 
@@ -325,7 +326,9 @@ function App() {
                 <span>Token</span>
                 <span className="label-text-alt self-end">
                   Balance:{" "}
-                  {balance && decimals ? formatUnits(balance, decimals) : null}
+                  {balance !== undefined && decimals
+                    ? formatUnits(balance, decimals)
+                    : null}
                 </span>
               </div>
               <div className="join">
