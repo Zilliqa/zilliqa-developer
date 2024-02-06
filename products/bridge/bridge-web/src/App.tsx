5a38eced8924dfcc38a80398ec90b55328787cfa
Bruno Wu <wubruno@outlook.com>
2024-02-06 13:15:04 +0000

    Add loading + fix bsc -> zil consecutive txn issue

--- products/bridge/bridge-web/src/App.tsx
+++ products/bridge/bridge-web/src/App.tsx
@@ -19,10 +19,13 @@ import {
   useNetwork,
   usePrepareContractWrite,
   useSwitchNetwork,
+  useWaitForTransaction,
 } from "wagmi";
 import { formatUnits, parseUnits } from "viem";
 import { MintAndBurnTokenManagerAbi } from "./abi/MintAndBurnTokenManager";
-import { toast } from "react-toastify";
+import { Id, toast } from "react-toastify";
+
+type TxnType = "approve" | "bridge";
 
 function App() {
   const [fromChain, setFromChain] = useState<Chains>(
@@ -37,6 +40,8 @@ function App() {
   const toChainConfig = chainConfigs[toChain]!;
   const { switchNetwork } = useSwitchNetwork();
   const { chain } = useNetwork();
+  const [latestTxn, setLatestTxn] = useState<[TxnType, `0x${string}`]>();
+  const [loadingId, setLoadingId] = useState<Id>();
 
   const [recipient, setRecipient] = useState<string>();
   const [token, selectedToken] = useState<TokenConfig>(
@@ -169,6 +174,7 @@ function App() {
     functionName: "approve",
     gas: fromChainConfig.isZilliqa ? 400_000n : undefined,
     type: fromChainConfig.isZilliqa ? "legacy" : "eip1559",
+    enabled: hasEnoughAllowance,
   });
 
   const { writeAsync: approve, isLoading: isLoadingApprove } =
@@ -182,6 +188,98 @@ function App() {
     (fromChainConfig.isZilliqa
       ? !isLoadingBridgeFromZilliqa
       : !isLoadingBridge);
+
+  const {
+    data: txnReceipt,
+    isLoading: isWaitingForTxn,
+    error,
+    refetch,
+  } = useWaitForTransaction({
+    hash: latestTxn?.[1],
+    enabled: !!latestTxn?.[1],
+  });
+
+  useEffect(() => {
+    if (error) {
+      // Little hack to get Zilliqa to refetch the pending txns
+      refetch();
+    }
+  }, [error, refetch]);
+
+  useEffect(() => {
+    if (txnReceipt && loadingId && latestTxn) {
+      let description;
+      if (latestTxn[0] === "bridge") {
+        description = (
+          <div>
+            Bridge request txn sent. From {fromChainConfig.name} to{" "}
+            {toChainConfig.name} {amount} {token.name} tokens. View on{" "}
+            <a
+              className="link text-ellipsis w-10"
+              onClick={() =>
+                window.open(
+                  `${fromChainConfig.blockExplorer}${txnReceipt.transactionHash}`,
+                  "_blank"
+                )
+              }
+            >
+              block explorer
+            </a>
+          </div>
+        );
+        setAmount(0);
+      } else if (latestTxn[0] === "approve") {
+        description = (
+          <div>
+            Approve txn successful. View on{" "}
+            <a
+              className="link text-ellipsis w-10"
+              onClick={() =>
+                window.open(
+                  `${fromChainConfig.blockExplorer}${txnReceipt.transactionHash}`,
+                  "_blank"
+                )
+              }
+            >
+              block explorer
+            </a>
+          </div>
+        );
+      } else {
+        return;
+      }
+      toast.update(loadingId, {
+        render: description,
+        type: "success",
+        isLoading: false,
+      });
+      setLoadingId(undefined);
+      setLatestTxn(undefined);
+    }
+  }, [
+    isWaitingForTxn,
+    txnReceipt,
+    loadingId,
+    latestTxn,
+    fromChainConfig.name,
+    fromChainConfig.blockExplorer,
+    toChainConfig.name,
+    amount,
+    token.name,
+  ]);
+
+  useEffect(() => {
+    if (!loadingId && isWaitingForTxn && latestTxn) {
+      const id = toast.loading("Transaction being processed...");
+      setLoadingId(id);
+    }
+  }, [isWaitingForTxn, latestTxn, loadingId]);
+
+  const showLoadingButton =
+    isLoadingBridgeFromZilliqa ||
+    isLoadingBridge ||
+    isLoadingApprove ||
+    isWaitingForTxn;
 
   return (
     <>
@@ -386,34 +484,19 @@ function App() {
               {!hasEnoughAllowance && hasEnoughBalance ? (
                 <button
                   className="btn w-5/6 mx-10 btn-outline"
-                  disabled={isLoadingApprove}
+                  disabled={showLoadingButton}
                   onClick={async () => {
                     if (approve) {
                       const tx = await approve();
-                      toast.success(
-                        <div>
-                          Approve txn sent. View on{" "}
-                          <a
-                            className="link text-ellipsis w-10"
-                            onClick={() =>
-                              window.open(
-                                `${fromChainConfig.blockExplorer}${tx.hash}`,
-                                "_blank"
-                              )
-                            }
-                          >
-                            block explorer
-                          </a>
-                        </div>
-                      );
                       console.log(tx.hash);
+                      setLatestTxn(["approve", tx.hash]);
                     }
                   }}
                 >
-                  {isLoadingApprove ? (
+                  {showLoadingButton ? (
                     <>
                       <span className="loading loading-spinner"></span>
-                      loading
+                      Loading
                     </>
                   ) : (
                     "Approve"
@@ -422,43 +505,24 @@ function App() {
               ) : (
                 <button
                   className="btn w-5/6 mx-10 btn-primary text-primary-content"
-                  disabled={!canBridge}
+                  disabled={!canBridge || showLoadingButton}
                   onClick={async () => {
-                    let tx: { hash: string };
+                    let tx: { hash: `0x${string}` };
                     if (fromChainConfig.isZilliqa && bridgeFromZilliqa) {
                       tx = await bridgeFromZilliqa();
-                      console.log(tx.hash);
                     } else if (bridge) {
                       tx = await bridge();
-                      console.log(tx.hash);
                     } else {
                       return;
                     }
-                    toast.success(
-                      <div>
-                        Bridge request txn sent. From {fromChainConfig.name} to{" "}
-                        {toChainConfig.name} {amount} {token.name} tokens. View
-                        on{" "}
-                        <a
-                          className="link text-ellipsis w-10"
-                          onClick={() =>
-                            window.open(
-                              `${fromChainConfig.blockExplorer}${tx.hash}`,
-                              "_blank"
-                            )
-                          }
-                        >
-                          block explorer
-                        </a>
-                      </div>
-                    );
-                    setAmount(0);
+                    console.log(tx.hash);
+                    setLatestTxn(["bridge", tx.hash]);
                   }}
                 >
-                  {isLoadingBridge || isLoadingBridgeFromZilliqa ? (
+                  {showLoadingButton ? (
                     <>
                       <span className="loading loading-spinner"></span>
-                      loading
+                      Loading
                     </>
                   ) : (
                     "Bridge"
