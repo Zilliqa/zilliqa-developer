61d78819ebea5b16502c2818f5aca3971616f1e8
Bruno Wu <wubruno@outlook.com>
2024-02-14 14:30:53 +0000

    Update tests

--- products/bridge/smart-contracts/script/zq/deployChainGateway.s.sol
+++ products/bridge/smart-contracts/script/zq/deployChainGateway.s.sol
@@ -5,7 +5,6 @@ import {Script} from "forge-std/Script.sol";
 import {Relayer} from "contracts/core/Relayer.sol";
 import {ValidatorManager} from "contracts/core/ValidatorManager.sol";
 import {ChainGateway} from "contracts/core/ChainGateway.sol";
-import {Target} from "foundry/test/Target.sol";
 import "forge-std/console.sol";
 
 contract Deployment is Script {
