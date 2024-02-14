f6a4c36a5282e48d662db497e586732fe1794b01
Bruno Wu <wubruno@outlook.com>
2024-02-14 15:35:39 +0000

    Add validatormanager tests & add ownable2step

--- products/bridge/smart-contracts/test/core/ValidatorManager.t.sol
+++ products/bridge/smart-contracts/test/core/ValidatorManager.t.sol
@@ -2,7 +2,81 @@
 pragma solidity 0.8.20;
 
 import {Tester} from "test/Tester.sol";
+import {ValidatorManager} from "contracts/core/ValidatorManager.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ValidatorManagerTests is Tester {
-    function test_happyPath() external {}
+    address owner = vm.createWallet("Owner").addr;
+    address validator1 = vm.createWallet("Validator1").addr;
+    address validator2 = vm.createWallet("Validator2").addr;
+    ValidatorManager validatorManager;
+
+    function setUp() external {
+        validatorManager = new ValidatorManager(owner);
+        address[] memory validators = new address[](1);
+        validators[0] = validator1;
+
+        vm.prank(owner);
+        validatorManager.initialize(validators);
+    }
+
+    function test_addValidator() external {
+        vm.startPrank(owner);
+        validatorManager.addValidator(validator2);
+
+        assertEq(validatorManager.validatorsSize(), 2);
+        assertEq(validatorManager.isValidator(validator1), true);
+        assertEq(validatorManager.isValidator(validator2), true);
+        vm.stopPrank();
+    }
+
+    function test_removeValidator() external {
+        vm.startPrank(owner);
+        validatorManager.removeValidator(validator1);
+
+        assertEq(validatorManager.validatorsSize(), 0);
+        assertEq(validatorManager.isValidator(validator1), false);
+        vm.stopPrank();
+    }
+
+    function test_revertAddValidatorIfNotOwner() external {
+        address nonOwner = vm.createWallet("NonOwner").addr;
+
+        vm.prank(nonOwner);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                nonOwner
+            )
+        );
+        validatorManager.addValidator(validator2);
+    }
+
+    function test_revertRemoveValidatorIfNotOwner() external {
+        address nonOwner = vm.createWallet("NonOwner").addr;
+
+        vm.prank(nonOwner);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                nonOwner
+            )
+        );
+        validatorManager.removeValidator(validator2);
+    }
+
+    function test_transferOwnership() external {
+        address newOwner = vm.createWallet("NewOwner").addr;
+
+        vm.startPrank(owner);
+        validatorManager.transferOwnership(newOwner);
+        // Ownership should only be transferred after the 2-step process
+        assertEq(validatorManager.owner(), owner);
+
+        vm.startPrank(newOwner);
+        validatorManager.acceptOwnership();
+        assertEq(validatorManager.owner(), newOwner);
+
+        vm.stopPrank();
+    }
 }
