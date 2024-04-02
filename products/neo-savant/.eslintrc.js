0ea5eda325ce94fcd4e86bfe2dd4fab36e24b074
Saeed Dadkhah <saeed@zilliqa.com>
2024-04-02 17:24:34 +0330

    Fix eslint

--- products/neo-savant/.eslintrc.js
+++ products/neo-savant/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended"],
   rules: {
-    "no-console": import.meta.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": import.meta.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
 };
