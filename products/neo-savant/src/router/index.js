a12561b076afe14827bbc0a328ca881b2f5ff58b
Saeed Dadkhah <saeed@zilliqa.com>
2024-04-04 17:45:31 +0330

    Fix style

--- products/neo-savant/src/router/index.js
+++ products/neo-savant/src/router/index.js
@@ -31,8 +31,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import("../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
 ];
 
