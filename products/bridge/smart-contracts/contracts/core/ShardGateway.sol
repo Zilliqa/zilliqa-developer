82d2d3231ba1dda68345922f306e6270a3b02144
Richard Watts <richard@zilliqa.com>
2024-06-28 15:49:53 +0100

    (feat) remove xbridge code from here and add a README.md to say where it has moved to.

--- products/bridge/smart-contracts/contracts/core/ShardGateway.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity ^0.8.20;
-
-import {Relayer} from "contracts/core/Relayer.sol";
-import {ShardDispatcher} from "contracts/core/ShardDispatcher.sol";
-
-contract ShardGateway is Relayer, ShardDispatcher {
-    constructor() Relayer(msg.sender) {}
-}
